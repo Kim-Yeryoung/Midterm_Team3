8ac1735999adcd978dbcfe2818b0ca90400a4c09
Yeryoung Kim <kimye6418@gmail.com>
2025-04-28 10:57:57 +0900

    update

--- midterm prob2.py
+++ midterm prob2.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
 #파일 넣기기
-input_file = "C:\Users\kimye\Desktop\2_Card.csv"
+input_file = "C:/Users/kimye/Desktop/2_Card.csv"
 
 def some_function(input_file):
 
@@ -78,98 +78,10 @@ def some_function(input_file):
                 onehot_cols.append(col)
         
 
-    #2-2. 중복 제거
-
-    # 1. 불필요한 열 제거
-    df = df.drop_duplicates()
-    df = df.drop(columns=['sofifa_id', 'player_url', 'dob', 'real_face', 'joined', 'contract_valid_until'])
-    
-
-    
-    # 처리할 컬럼 리스트
-    position_cols = [
-        'ls', 'st', 'rs', 'lw', 'lf', 'cf', 'rf', 'rw',
-        'lam', 'cam', 'ram', 'lm', 'lcm', 'cm', 'rcm', 'rm',
-        'lwb', 'ldm', 'cdm', 'rdm', 'rwb', 'lb', 'lcb', 'cb', 'rcb', 'rb'
-    ]
-
-    # +를 기준으로 앞 숫자만 남기기
-    for col in position_cols:
-        df[col] = df[col].astype(str).str.split('+').str[0]  # +기준 분리해서 앞부분만
-        df[col] = pd.to_numeric(df[col], errors='coerce')    # 숫자로 변환
-    
-    
-    #4-2. 표준화화
-    inmax_cols = [
-        'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic',
-        'gk_diving', 'gk_handling', 'gk_kicking', 'gk_reflexes', 'gk_speed', 'gk_positioning',
-        'attacking_crossing', 'attacking_finishing', 'attacking_heading_accuracy',
-        'attacking_short_passing', 'attacking_volleys',
-        'skill_dribbling', 'skill_curve', 'skill_fk_accuracy', 'skill_long_passing', 'skill_ball_control',
-        'movement_acceleration', 'movement_sprint_speed', 'movement_agility', 'movement_reactions', 'movement_balance',
-        'power_shot_power', 'power_jumping', 'power_stamina', 'power_strength', 'power_long_shots',
-        'mentality_aggression', 'mentality_interceptions', 'mentality_positioning',
-        'mentality_vision', 'mentality_penalties', 'mentality_composure',
-        'defending_marking', 'defending_standing_tackle', 'defending_sliding_tackle',
-        'ls', 'st', 'rs', 'lw', 'lf', 'cf', 'rf', 'rw',
-        'lam', 'cam', 'ram', 'lm', 'lcm', 'cm', 'rcm', 'rm',
-        'lwb', 'ldm', 'cdm', 'rdm', 'rwb', 'lb', 'lcb', 'cb', 'rcb', 'rb'
-    ]
-    df[inmax_cols]=normalize_numerics(df[inmax_cols])
-    
-    
-    # 변수 분류 (OneHot & gender or Label)
-    label_encode_cols = []
-    onehot_cols = []
-
-    for col in df.columns:
-        if df[col].dtype == 'object' and col not in ['Gender', 'gender', 'Sex', 'sex']:
-            n_unique = df[col].nunique()
-            if n_unique > 2:
-                label_encode_cols.append(col)
-            elif n_unique == 2:
-                onehot_cols.append(col)
-
-    
-    #3. 엔코딩(변수 분류): #성별 주의!
-    onehot_cols, label_encode_cols
-
-    df[label_encode_cols]=encode_categoricals(df[label_encode_cols]) # Label
-    df = pd.get_dummies(df, columns=onehot_cols ) #OneHot
-
-    def age_to_category(age):
-        if age <= 21:
-            return 'young'
-        elif age <= 26:
-            return 'prime'
-        else:
-            return 'veteran'
-
-    df['age_category'] = df['age'].apply(age_to_category)
-
-    # 2. BMI 계산 (kg/m^2)
-    df['body_mass_index'] = df['weight_kg'] / ((df['height_cm'] / 100) ** 2)
-
-        # 3. 공격 능력 평균
-    df['attacking_ability'] = (df['shooting'] + df['passing'] + df['dribbling']) / 3
-
-    # 4. 수비 능력 평균
-    df['defending_ability'] = (df['defending'] + df['physic']) / 2
-
-        # 1. 몸값 대비 주급 비율
-    df['value_to_wage_ratio'] = df['value_eur'] / df['wage_eur']
-
-    # 2. 바이아웃/시장가치 비율
-    df['release_clause_ratio'] = df['release_clause_eur'] / df['value_eur']
-
-    # 3. 연봉 (주급 × 52주)
-    df['annual_wage'] = df['wage_eur'] * 52
 
  
     return df
 
-output_file = some_function(input_file) 
 
-df.to_csv('output_file.csv')
+df
 
-output_file
