6874df289e124d3c7cf50468fb83d76c7091a86f
Rkdtks-Lee <bca34781@handong.ac.kr>
2025-04-28 10:16:17 +0900

    push

--- midterm prob1.py
+++ midterm prob1.py
@@ -6,12 +6,59 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder  # 정규화 및 
 df = pd.read_csv("1_adults.csv", encoding='cp949')
 print(df.info())
 
+for col in df.columns:
+    df[col] = df[col].replace('?', np.nan)
 
 def handle_missing(df):
-    for col in df.columns:
-        if (df[col] == 0).all() or df[col].isnull().all():
-            df.drop(columns=[col], inplace=True)
     num_columns = df.select_dtypes(include=['float64', 'int64']).columns
     for col in num_columns:
         df[col] = df[col].fillna(df[col].median())
+    
+    cat_cols = df.select_dtypes(include=['object', 'category']).columns
+    for col in cat_cols:
+        df[col] = df[col].fillna(df[col].mode().iloc[0])
+
     return df
+
+def remove_outliers(df):
+    df_cleaned = df.copy()
+    num_cols = df.select_dtypes(include=['float64', 'int64']).columns
+
+    for col in num_cols:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df_cleaned = df_cleaned[(df_cleaned[col] >= lower_bound) & (df_cleaned[col] <= upper_bound)]
+        
+    return df_cleaned
+
+def encode_categoricals(df):
+    cat_cols = df.select_dtypes(include=['object', 'category']).columns
+    print("Categorical columns identified:", cat_cols)
+    for col in cat_cols:
+        df[col] = LabelEncoder().fit_transform(df[col])
+    return df
+
+def create_features(df):
+    df['age_group'] = pd.cut(df['age'], bins=[0, 30, 60, 120], labels=['young', 'adult', 'senior'])
+    return df
+
+def normalize_numerics(df):
+    num_cols = df.select_dtypes(include=['float64', 'int64']).columns
+    scaler = StandardScaler()
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+    return df
+
+def preprocessing(df):
+    df = handle_missing(df)
+    df = remove_outliers(df)
+    df = create_features(df)
+    df = encode_categoricals(df)
+    df = normalize_numerics(df)
+    return df
+
+df = preprocessing(df)
+df.to_csv('processed_1_adults.csv', index = False)
+print(df.info())
