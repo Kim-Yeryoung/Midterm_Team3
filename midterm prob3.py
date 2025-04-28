bbef5bccfb88f9bba81b342afab516820082c345
cjsnhope <cjsnhope@gmail.com>
2025-04-28 10:55:38 +0900

    update

--- midterm prob3.py
+++ midterm prob3.py
@@ -4,15 +4,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
-
-def handle_missing(df):
-    if df.dtype in ['float64', 'int64']:  # 수치형
-        df = df.fillna(df.median())
-    else:  # 범주형 또는 문자열
-        df = df.fillna(df.mode().iloc[0])
-    return df
-
-
 def prepro3(input_file):
 
     df_raw = pd.read_csv(input_file)
@@ -21,18 +12,16 @@ def prepro3(input_file):
     #print(df['longitude'].value_counts())
     #null_ratio = (df.isnull().sum() / len(df)) * 100
     df['id'] = df['id'].drop_duplicates()
-    #열 관계 활용
+    
     df['name'] = df['name'].fillna(df.groupby('id')['name'].transform('first'))
     df['host_name'] = df['host_name'].fillna(df.groupby('id')['host_name'].transform('first'))
 
     df['neighbourhood_group', 'neighbourhood'] = pd.get_dummies(df['neighbourhood_group', 'neighbourhood'])
     df['room_type'] = df['room_type'].map({'Shared room': 1, 'Private room': 2, 'Entire home/apt': 3})
 
-
-
+    scaler = MinMaxScaler()
+    df['reserv_ava'] = scaler.fit_transform(df['availability_365'])
     df['profitability'] = df['price'] * df['minimum_nights'] * df['room_type']
-    
-
 
     return 'result3.csv'
 
